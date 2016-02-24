21364c70714134651b6b6ea96ba28161482bfa0b
David Prentiss <dprentiss@gmail.com>
2016-02-24 16:06:01 -0500

    virtue cards are jQuery sortable

--- js/desiderata.js
+++ js/desiderata.js
@@ -2,3 +2,8 @@ var todoList = ["submit paper", "reschedule car repair"];
 var projectList = ["Evacuation Paper"];
 var aphorisms = [];
 var habits = [];	
+
+$(function() {
+    $( ".virtue-list" ).sortable();
+    $( ".virtue-list" ).disableSelection();
+  });
