b0139610e82af706a0371e2765b1f4a824f0a222
David Prentiss <dprentiss@gmail.com>
2016-03-05 09:27:22 -0500

    updated virtues array

--- js/desiderata.js
+++ js/desiderata.js
@@ -1,9 +1,14 @@
 var todoList = ["submit paper", "reschedule car repair"];
+var virtues = [
+	{"Compassion": "Do unto others as you would have them do unto you"},
+	{"Integrity":"Do the right thing, even when no one is watching."},
+	{"Excellence": "I do the very best I know how, the very best I can, and I mean to keep on doing so until the end."}
+	];
 var projectList = ["Evacuation Paper"];
 var aphorisms = [];
 var habits = [];	
 
 $(function() {
-    $( ".virtue-list" ).sortable();
-    $( ".virtue-list" ).disableSelection();
-  });
+	$(".virtue-list").sortable();
+	$(".virtue-list").disableSelection();
+});
